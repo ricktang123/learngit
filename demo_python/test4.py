a328c82d37f43fd9950ff92c5239e504341905f1
epqtuuz <hao.jing@outlook.com>
2016-10-14 17:31:12 +0800

    further

--- demo_python/test4.py
+++ demo_python/test4.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from Tkinter import *
 def anti_vowel(text):
     textnew=''
     for i in text:
@@ -8,3 +9,23 @@ def anti_vowel(text):
 
 print anti_vowel('please')
 
+top= Tk()
+label=Label(top, text="Welcome to TA")
+label.pack()
+
+button = Button(top, text="Quit", command=top.quit, bg='yellow', fg='red')
+button.pack()
+
+frm =  Frame(top)
+#left
+frm_L = Frame(frm)
+Label(frm, text='Left is good').pack(side=TOP)
+#frm_L.pack()
+frm.pack()
+var=StringVar()
+e=Entry(top,textvariable=var)
+var.set('Test is a test')
+e.pack()
+
+
+top.mainloop()
